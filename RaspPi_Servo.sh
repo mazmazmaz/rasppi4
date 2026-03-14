2efcf2c58ee85b763c4c00638c20157c2517138d
Mazi Memarzadeh <mazmazmaz@me.com>
2026-03-13 20:25:16 -0700

    Add files via upload

---
+++ RaspPi_Servo.sh
@@ -0,0 +1,20 @@
+apt-get -y install build-essential
+apt-get -y install cmake
+apt-get -y install pkg-config
+apt-get -y install libpng12-0 libpng12-dev libpng++-dev libpng3
+apt-get -y install libpnglite-dev libpngwriter0-dev libpngwriter0c2
+apt-get -y install zlib1g-dbg zlib1g zlib1g-dev
+apt-get -y install libjasper-dev libjasper-runtime libjasper1
+apt-get -y install pngtools libtiff4-dev libtiff4 libtiffxx0c2 libtiff-tools
+apt-get -y install libjpeg8 libjpeg8-dev libjpeg8-dbg libjpeg-prog
+apt-get -y install ffmpeg libavcodec-dev libavcodec52 libavformat52 libavformat-dev
+apt-get -y install libgstreamer0.10-0-dbg libgstreamer0.10-0  libgstreamer0.10-dev
+apt-get -y install libxine1-ffmpeg  libxine-dev libxine1-bin
+apt-get -y install libunicap2 libunicap2-dev
+apt-get -y install libdc1394-22-dev libdc1394-22 libdc1394-utils
+apt-get -y install swig
+apt-get -y install libv4l-0 libv4l-dev
+apt-get -y install python-numpy
+apt-get -y install libpython2.6 python-dev python2.6-dev
+apt-get -y install libjpeg-progs libjpeg-dev
+apt-get -y install libgstreamer-plugins-base0.10-dev
